610e6aec552ed7696c19b89557821da755d5d9dc
demian garcia <dag@demians.org>
2025-08-09 05:01:44 -0700

    locationSearch: restore git history

--- flight-booking-system/shared-code/util/locationSearch.js
+++ flight-booking-system/shared-code/util/locationSearch.js
@@ -1,7 +1,18 @@
 // locationsearch.js
 // This module provides functionality to search for location destinations.
 let inputstring = "";
-const exampleLocations = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+const exampleLocations = ["Tokyo", "Delhi" ,"Shanghai", "São_Paulo" ,"Mexico City", 
+    "Cairo Mumbai", "Beijing", "Dhaka", "Osaka", "New York City", "Tehran", "Karachi", 
+    "Kolkata", "Buenos Aires", "Chongqing", "Istanbul", "Manila", "Lagos","Rio de Janeiro"
+, "Tianjin", "Kinshasa", "Guangzhou", "Los Angeles", "Moscow", "Shenzhen", "Lahore"
+, "Bengaluru", "Paris", "Bogotá", "Jakarta", "Chennai", "Lima", "Bangkok", "Seoul", "Nagoya"
+, "Hyderabad", "London", "Chicago", "Chengdu", "Nanjing", "Wuhan", "Ho Chi Minh City"
+, "Luanda", "Ahmedabad", "Kuala Lumpur", "Xi'an", "Hong Kong", "Dongguan", "Hangzhou"
+, "Foshan", "Shenyang", "Riyadh", "Baghdad", "Santiago", "Surat", "Madrid", "Suzhou", "Pune"
+, "Harbin", "Houston", "Dallas", "Toronto", "Dar es Salaam", "Miami", "Belo Horizonte"
+, "Singapore", "Philadelphia", "Atlanta", "Fukuoka", "Khartoum", "Barcelona", "Johannesburg"
+, "Saint Petersburg", "Qingdao", "Dalian", "Washington", "Yangon", "Alexandria", "Jinan"
+, "Guadalajara"];
 export function locationSearch(input) 
 {
     inputstring = input;
